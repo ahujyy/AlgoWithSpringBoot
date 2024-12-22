6b8cfdd6e4dc9caeae8c33b1290c69a2628740a4
ahujyy <305048687@qq.com>
2024-12-22 22:51:06 +0800

    rm build and .idea .gradle

--- build/resources/main/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO TASK (NAME, DESCRIPTION ) VALUES ('task1', 'first task');
-INSERT INTO TASK (NAME, DESCRIPTION ) VALUES ('task2', 'second task');
-INSERT INTO TASK (NAME, DESCRIPTION ) VALUES ('task3', 'third task');
-INSERT INTO TASK (NAME, DESCRIPTION ) VALUES ('task4', 'fourth task');
--- build/resources/main/schema.sql
+++
@@ -1,7 +0,0 @@
-create table TASK(
-  ID int not null AUTO_INCREMENT,
-  NAME varchar(100) not null,
-  DESCRIPTION varchar(200) not null,
-  STATUS int default 1,
-  PRIMARY KEY ( ID )
-);
